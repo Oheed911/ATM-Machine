49be7590bb4739147300250af1b4694784e672c7
oheed911 <oheedimran@outlook.com>
2021-05-09 04:23:13 +0500

    MAin of the program

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+OHEED IMRAN 
+CS-D
+19I-0525
+
+*/
+#include "Engine.h"
+int main()
+{
+    //Creating the object of the Engine class
+    Engine EngineOfProgram;
+    // Now calling the function which included the whole program
+    EngineOfProgram.Display_FirstOptions();
+    //end of the program
+    cout <<endl;
+    return 0;
+} 
